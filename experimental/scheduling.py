0b7cbf419a71b14bf545d245d88e3559bac11185
Heiko Kulinna <Heikokulinna@gmx.de>
2022-09-29 22:32:00 +0200

    [HK] experimental: gantt diagram

---
+++ experimental/scheduling.py
@@ -0,0 +1,189 @@
+
+
+
+
+import plotly.express as px
+import pandas as pd
+import plotly
+import numpy as np
+
+
+
+
+
+df = pd.DataFrame([
+    dict(Task="Job A", Start='2009-01-01', Finish='2009-02-28', Completion_pct=50),
+    dict(Task="Job B", Start='2009-03-05', Finish='2009-04-15', Completion_pct=25),
+    dict(Task="Job C", Start='2009-02-20', Finish='2009-05-30', Completion_pct=75)
+])
+
+
+df
+
+fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color="Completion_pct")
+fig.update_yaxes(autorange="reversed")
+
+
+df = pd.DataFrame([
+    dict(Project="Project A", Team="Heiko", Start="2022-01-15", Finish="2022-12-15", Working_hours=400),
+    dict(Project="Project B", Team="Ralf", Start="2022-04-15", Finish="2022-09-30", Working_hours=300),
+    dict(Project="Project A", Team="Ralf", Start="2022-01-15", Finish="2022-12-15", Working_hours=200),
+    dict(Project="Project C", Team="Tobi", Start="2022-06-15", Finish="2022-11-01", Working_hours=350),
+])
+
+df
+
+
+fig = px.timeline(df, x_start="Start", x_end="Finish", y="Project", color="Working_hours")
+fig.update_yaxes(autorange="reversed")
+
+
+fig = px.timeline(df, x_start="Start", x_end="Finish", y="Project", color="Team")
+
+fig = px.timeline(df, x_start="Start", x_end="Finish", y="Team", color="Project")
+
+fig = px.timeline(df, x_start="Start", x_end="Finish", y=["Team", "Project"], color="Working_hours")
+
+
+fig.update_xaxes(range=["2022-01", "2023-01"])
+
+
+plotly.offline.plot(fig)
+
+
+
+
+import plotly.figure_factory as ff
+
+
+# Task, Start, Finish
+df
+
+df = pd.DataFrame([
+    dict(Task="Task A", Team="Heiko", Start="2022-01-15", Finish="2022-06-15", Working_hours=400),
+    dict(Task="Task B", Team="Ralf", Start="2022-04-15", Finish="2022-09-30", Working_hours=300),
+    dict(Task="Task A", Team="Ralf", Start="2022-06-15", Finish="2022-12-15", Working_hours=200),
+    dict(Task="Task C", Team="Tobi", Start="2022-06-15", Finish="2022-11-01", Working_hours=350),
+])
+df
+
+df["Task_Team"] = df["Task"]+df["Team"]
+
+
+
+
+# build a colormap for status 
+colormap = {s:c for s,c in zip(df["Task"].unique(), px.colors.qualitative.Plotly)}
+colormap
+
+
+df
+
+df = df.sort_values(by="Task")
+df
+
+fig = ff.create_gantt(df, index_col='Task', group_tasks=True)
+fig = ff.create_gantt(df, index_col='Task', group_tasks=False)
+
+
+# fig = ff.create_gantt(df, index_col='Task', colors="Team", group_tasks=False)
+fig = ff.create_gantt(df, index_col='Task', colors=colormap, group_tasks=False)
+
+fig = ff.create_gantt(df, index_col='Team', group_tasks=True)
+
+fig = ff.create_gantt(df, index_col='Task_Team', group_tasks=True)
+
+fig = ff.create_gantt(df, index_col='Task_Team', group_tasks=False)
+
+
+date = "2022-10-13"
+fig.add_vline(x=date, line_width=3, line_color="black")
+
+
+plotly.offline.plot(fig)
+
+
+
+
+####################
+
+
+
+
+df = pd.DataFrame([
+    dict(Task="Task A", Team="Heiko", Start="2022-01-15", Finish="2022-06-15", Working_hours=400),
+    dict(Task="Task B", Team="Ralf", Start="2022-04-15", Finish="2022-09-30", Working_hours=300),
+    dict(Task="Task A", Team="Ralf", Start="2022-04-15", Finish="2022-12-15", Working_hours=200),
+    dict(Task="Task C", Team="Tobi", Start="2022-06-15", Finish="2022-11-01", Working_hours=350),
+])
+df
+
+df = df.sort_values(by="Task")
+df = df.reset_index(drop = True)
+df
+
+colormap = {s:c for s,c in zip(df["Team"].unique(), px.colors.qualitative.Plotly)}
+colormap
+
+df["Team"]
+list(df["Team"])
+# ['Heiko', 'Ralf', 'Ralf', 'Tobi']
+[colormap[i] for i in df["Team"]]
+# ['#636EFA', '#EF553B', '#EF553B', '#00CC96']
+
+
+fig = ff.create_gantt(df, index_col='Task', colors=[colormap[i] for i in df["Team"]], group_tasks=False)
+plotly.offline.plot(fig)
+
+
+{i: colormap[df.loc[i,"Team"]] for i in df.index}
+
+df["Task"]
+
+df["index"] = df.index 
+
+# fig = ff.create_gantt(df, index_col='Task', colors=colormap, group_tasks=False)
+# plotly.offline.plot(fig)
+
+
+fig = ff.create_gantt(df, index_col='Task', colors={i: colormap[df.loc[i,"Team"]] for i in df.index}, group_tasks=False)
+plotly.offline.plot(fig)
+
+
+df
+fig = ff.create_gantt(df, index_col="index", task_names = "Task", colors={i: colormap[df.loc[i,"Team"]] for i in df.index}, group_tasks=False)
+plotly.offline.plot(fig)
+
+
+
+# Das hier !
+
+list(df["Team"])
+# ['Heiko', 'Ralf', 'Ralf', 'Tobi']
+df
+
+# da wo der Wert sich ändert
+v = list(df["Task"])
+v = np.array(v)
+v
+list(np.where(v[:-1] != v[1:])[0])
+# [1, 2]
+
+
+
+fig = ff.create_gantt(df, index_col="Team", task_names = "Task",  group_tasks=False, show_colorbar=True)
+fig.update_yaxes(autorange="reversed")
+fig.add_hline(y = 2.5, line_width=3, line_color="white")
+fig.add_hline(y = 1.5, line_width=3, line_color="white")
+fig.add_vline(x=date, line_width=3, line_color="black")
+plotly.offline.plot(fig)
+
+
+
+fig = ff.create_gantt(df, index_col="Team", task_names = "Task",  group_tasks=True)
+fig.update_yaxes(autorange="reversed")
+plotly.offline.plot(fig)
+
+
+
+
